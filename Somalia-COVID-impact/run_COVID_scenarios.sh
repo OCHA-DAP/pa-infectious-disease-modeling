87711d3638b541f81d5314693e854be3c016a025
Leonardo Milano <milanoleonardo@yahoo.it>
2021-03-30 11:29:18 +0200

    bash script and new results for COVID scenario

---
+++ Somalia-COVID-impact/run_COVID_scenarios.sh
@@ -0,0 +1,8 @@
+python ../Cholera.py Inputs_COVID_Cholera_National.xlsx -o . -i
+mv Cholera.xlsx Results_COVID_Cholera_National.xlsx
+
+python ../Malaria.py Inputs_COVID_Malaria_National.xlsx -o . -i
+mv Malaria.xlsx Results_COVID_Malaria_National.xlsx
+
+python ../Measles.py Inputs_COVID_Measles_National.xlsx -o . -i
+mv Measles.xlsx Results_COVID_Measles_National.xlsx
